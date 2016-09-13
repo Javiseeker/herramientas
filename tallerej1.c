dc8b924c9794e2090341ad8c2fab451c519968ce
Javier ALonso <javiseeker.javieralonso123@outlook.es>
2016-09-13 08:18:19 -0500

    se agrega el primer ejercicio del taller 13/09/16

---
+++ tallerej1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int a, b;
+printf("Numero 1:");
+scanf("%d",&a);
+printf("Numero 2:");
+scanf("%d",&b);
+if(a>b)
+printf(" el numero mayor es:%d", a);
+
+else
+printf(" el numero mayor es:%d", b);
+return 0;
+
+}
